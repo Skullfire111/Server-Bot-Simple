fe327cdd2633bfada13e38de8de4ff3927d61d68
Skullfire111 <36246821+Skullfire111@users.noreply.github.com>
2018-02-13 18:13:53 -0500

    Update v_of_torf.py

--- v_of_torf.py
+++ v_of_torf.py
@@ -1,1 +1,13 @@
+import discord
+import asyncio
+from discord.ext import commands
 
+async def torfv(torf):
+	for torf==change:
+		if t==True:
+			t = False
+			return False	
+		if t==False:
+			t = True
+			return True
+	break
