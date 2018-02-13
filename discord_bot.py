d9230e32b8886b08e3ca715ca3cfbf0c084c6102
Skullfire111 <36246821+Skullfire111@users.noreply.github.com>
2018-02-13 15:59:21 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -7,31 +7,49 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix= '>')
 
 @bot.command(pass_context=True)
-async def member(ctx):
-	if ctx = member
+async def role(ctx):
+	if ctx = message.author
 		user = message.author
 		for member.roles:
 			if role.name != "<role name>":
 				await bot.say("Requesting promotion. Please wait for approval")
 				for role.name(<moderator>)
-					await client.send_message(privat_mod_room, content=user "is requesting the <role name> role", *, tts=False, embed=None)
+					await client.send_message(channel.privat_mod_room, content=user "is requesting the <role name> role", *, tts=False, embed=None)
 			else await bot.say("You are already a <role>!")
 
 @bot.command(pass_context=True)
 @checks.admin_or_permissions(manage_roles=True)
-async def setmemb(ctx, member, yorn):
+async def role(ctx, user, yorn):
 	if setmemb = ctx:
 		if yorn = yes:
-			add_roles(member, <role>)
-			await client.send_message(member, content="Your request for <role name> has been granted!", *, tts=False, embed=None)
+			add_roles(user, <role>)
+			await client.send_message(user, content="Your request for <role name> has been granted!", *, tts=False, embed=None)
 		elif yorn = no:
-			await client.send_message(member, content="Sorry, but your request for <role name> has been denied.", *, tts=False, embed=None)
+			await client.send_message(user, content="Sorry, but your request for <role name> has been denied.", *, tts=False, embed=None)
 	
 @bot.command(pass_context=True)
-async def meme(ctx)
-	if ctx = meme
-		await client.send_message(message.channel, content="Wow, a random me appeared from */r/memes*!", *, tts=False, embed=www.reddit.com/r/memes/random)
-
+async def message(ctx):
+	if ctx == rmeme:
+		await client.send_message(message.channel, content="Wow, a random meme appeared from */r/memes*!", *, tts=False, embed="www.reddit.com/r/memes/random")
+        if ctx == rrandom:
+		await client.send_message(message.channel, content="Get your daily dose of internet with a random *clean* post from Reddit!", *, tts=False, embed="www.reddit.com/random")
+        if ctx == rall:
+		await client.send_message(message.channel, content="Find your thing with recent top *clean* posts from **all** of Reddit!", *, tts=False, embed="www.reddit.com/random")
+        if ctx == r:
+		await bot.say("``````")
+         
+		
+@bot.command(pass_context=True)
+@checks.admin_or_permissions(manage_server=True)
+async def message(ctx):
+	while state == on:
+		await client.send_message(channel.memes, content=None, *, tts=False, embed="www.reddit.com/r/memes/random")
+	if ctx=memes:
+		if state == on:
+			state=off
+		if state == off:
+			state=on
+		
 @bot.command(pass_context=True)
 @checks.admin_or_permissions(mute_user=True)
 async def mute(ctx, user, hrs):
