78de2c4e7e80ebd731d8b7fff64e30081e535682
Skullfire111 <36246821+Skullfire111@users.noreply.github.com>
2018-03-31 19:49:06 -0400

    Update discord_bot_main.py

--- discord_bot_main.py
+++ discord_bot_main.py
@@ -1,6 +1,6 @@
 import discord
+import praw
 import random
-import mute
 import asyncio
 from discord.ext import commands
 
@@ -15,11 +15,86 @@ async def on_ready():
 	global pmr
 	global report
 	global torf
+	global ml
+	global mr
 
 	torf = False
-	pmr = "307110698322886658"
-	memes = "420307378093817856"
-	report = "410902743515922432"
+	pmr = 307110698322886658
+	memes = 420307378093817856
+	report = 410902743515922432
+	ml = 421900373507309569
+	mr = 429730346251190283
+	
+	mlc = bot.get_channel(ml)
+	lm = 'Logged in as **'
+	lm += bot.user.name
+	lm += ', '
+	lm += str(bot.user.id)
+	lm += '** using `'
+	lm += discord.__version__
+	lm += '`'
+	await mlc.send(lm)
+
+@bot.event
+async def on_message_delete(message):
+	mrc = bot.get_channel(mr)
+	channel = message.channel.mention
+
+	mc = 'Deleted Message in '
+	mc += channel
+	mc += ':'
+
+	em = discord.Embed(title=mc, description=message.content, colour=0xe74c3c)
+	em.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+	try:
+		if votearrow.content.startswith('https://'):
+			em.set_image(url=message.content)
+			breakstar = False
+	except:
+		pass
+	try:
+		attach = message.attachments
+		em.set_image(url = attach[0].url)
+		breakstar = False
+	except:
+		pass
+
+	await mrc.send(embed = em)
+
+@bot.event
+async def on_message_edit(message, after):
+	mrc = bot.get_channel(mr)
+	if after.channel is mrc:
+		pass
+	else:
+		channel = message.channel.mention
+
+		mc = 'Edited Message in '
+		mc += channel
+		mc += ':'
+
+		me = 'Old Message: \n \n'
+		me += message.content
+		me += '\n \n \n'
+		me += 'New Message: \n \n'
+		me += after.content
+
+		em = discord.Embed(title=mc, description=me, colour=0xFFD700)
+		em.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+		try:
+			if message.content.startswith('https://'):
+				em.set_image(url=message.content)
+				breakstar = False
+		except:
+			pass
+		try:
+			attach = message.attachments
+			em.set_image(url = attach[0].url)
+			breakstar = False
+		except:
+			pass
+
+		await mrc.send(embed = em)
 
 @bot.command(pass_context=True)
 async def h(ctx):
@@ -50,24 +125,33 @@ async def h(ctx):
 	helpm += ">mute: Used to temporarily mute any amount of hours" + "\n"
 	helpm += "       The number of hours you wish to mute someone must be an integer" + "\n"
 	helpm += "```"
-	await bot.say(helpms)
+	await ctx.send(helpm)
 
-@bot.command(pass_context=True)
+@bot.command()
 async def clap(ctx, *, sentence : str = None):
     if sentence is None:
-        await bot.say('You have to say something before this command works')
+        await ctx.send('You have to say something before this command works')
     else:
         sentence = sentence.replace(' ', ':clap:')
         sentence += ':clap:'
         await ctx.send(sentence)
 
+@bot.command()
+async def smh(ctx, *, headshake : str = None):
+	if headshake is None:
+		await ctx.send('You have to say something before this command works')
+	else:
+		headshake = headshake.replace('smh', 'shaking my smh')
+		headshake += ' smh'
+		await ctx.send(headshake)
+
 @bot.command(pass_context=True)
 async def role(ctx):
 	for role.name in member.roles:
 		if role.name != "Member":
-			await bot.say("You already are a Member!")
+			await ctx.send("You already are a Member!")
 		else:
-			await bot.say("Requesting promotion. Please wait for approval")
+			await ctx.send("Requesting promotion. Please wait for approval")
 			mm = message.author
 			mm+= 'is requesting to be a Member'
 			await client.send_message(pmr, mm)
@@ -75,20 +159,20 @@ async def role(ctx):
 @bot.command(pass_context=True)
 async def givememb(ctx, user = None, yorn = None):
 	if yorn is None:
-		await bot.say('Correct use of this function is: >setmemb <@person> <yes/no>')
+		await ctx.send('Correct use of this function is: >setmemb <@person> <yes/no>')
 	elif user is None:
-		await bot.say('Correct use of this function is: >setmemb <@person> <yes/no>')
+		await ctx.send('Correct use of this function is: >setmemb <@person> <yes/no>')
 	else:
 		if yorn is "yes":
 			add_roles(user, 'Member')
-			await bot.send_message(user, "Your request for Member has been granted!")
+			await user.send("Your request for Member has been granted!")
 		elif yorn is "no":
-			await bot.send_message(user, "Sorry, but your request for Member has been denied.")
+			await user.send("Sorry, but your request for Member has been denied.")
 		else:
-			await bot.say('You do not have permission to use this command!')
-	
+			await ctx.send('You do not have permission to use this command!')
+
 @bot.command()
-async def r(ctx, ri = None):
+async def r(ctx, ri : str = None):
 	if ri is None:
 		reddit = "```Use the >r branch of commands to bring up a post (or Subreddit) from Reddit:" + "\n"
 		reddit += ">r all: Recent top posts from all of Reddit (clean)" + "\n"
@@ -101,77 +185,99 @@ async def r(ctx, ri = None):
 		await ctx.send(reddit)
 	else:
 		if ri is "meme":
-			meme = "Wow, a random meme appeared from */r/me_irl*!\n"
-			em = "www.reddit.com/r/me_irl/random"
-			await ctx.send(message.channel, meme, embed=em)
+			subreddit = reddit.subreddit('me_irl')
+			submission = subreddit.get_random_submission
+			meme = submission.url
+			await ctx.send("Wow, a random meme appeared from */r/me_irl*!")
+			await ctx.send(meme)
 		elif ri is "random":
-			random = "Get your daily dose of internet with a random *clean* post from Reddit!\n"
-			em = "www.reddit.com/random"
-			await ctx.send(message.channel, random, embed=em)
+			subreddit = reddit.subreddit('all')
+			submission = subreddit.get_random_submission
+			random = submission.url
+			await ctx.send("Get your daily dose of internet with a random *clean* post from Reddit!")
+			await ctx.send(random)
 		elif ri is "all":
-			all = "Find your thing with recent top *clean* posts from **all** of Reddit!\n"
-			em = "www.reddit.com/all"
-			await ctx.send(message.channel, all, embed=em)
-	
+			await ctx.send("Find your thing with recent top *clean* posts from **all** of Reddit!")
+			subreddit = reddit.subreddit('all')
+			for i in subreddit.hot(limit = 5):
+				all = i.url
+				await ctx.send(all)
+
 @bot.command()
 @commands.has_permissions(manage_messages=True)
 async def memes(ctx):
+	memechannel = bot.get_channel(memes)
 	if torf is True:
-		bot.say('The \"Hourly Memes\" function has been toggled on.')
-		m = 3600
-		while m	> 0:
-			if m > 0:
-				await asyncio.sleep(1)
-				m = m - 1
-				if m == 0:
-					memechannel = bot.get_channel(memes)
-					em = "www.reddit.com/r/me_irl/random"
-					await bot.send_message(memechannel, embed=em)
-					m = 3600
+		ctx.send('The \"Hourly Memes\" function has been toggled on.')
 		torf = False
+		while True:
+			subreddit = reddit.subreddit('me_irl')
+			submission = subreddit.get_random_submission
+			meme = submission.url
+			await memechannel.send(meme)
+			await asyncio.sleep(3600)
 	else:
-		await bot.say('The \"Hourly Memes\" function has been toggled off.')
+		await ctx.send('The \"Hourly Memes\" function has been toggled off.')
 		torf = True
 		
 @memes.error
 async def memes_error(ctx, error):
 	if isinstance(error, commands.errors.MissingPermissions):
 		await ctx.send(':x: You do not have permission to use this command!')
-	
+
 @bot.command(pass_context=True)
 @commands.has_permissions(manage_messages=True)
-async def mute(ctx, user = None, tint = None, tdenom = None, reason = None):
-	await ctx.send('The command went through.')
+async def mute(ctx, user : discord.Member, tint :str = None, tdenom :str = None, *, reason : str = None):
 	if tdenom in ['s', 'm', 'h', 'd']:
-		if user is None:
+		if user is ' ':
 			await ctx.send('Correct usage is: >mute <@person> <time integer> <s/m/h/d> <reason(optional)>')
-		elif tint is None:
+		elif tint is ' ':
 			await ctx.send('Correct usage is: >mute <@person> <time integer> <s/m/h/d> <reason(optional)>')
-		elif tdenom is None:
+		elif tdenom is ' ':
 			await ctx.send('Correct usage is: >mute <@person> <time integer> <s/m/h/d> <reason(optional)>')
 		else:
 			role = discord.utils.get(ctx.guild.roles, name='Muted')
 			if role in user.roles:
-				await ctx.send(user, 'has already been muted')
+				umention = user.mention
+				already = umention
+				already += ' has already been muted'
+				await ctx.send(already)
 			else:
 				await user.add_roles(role)
-				muted = user
-				muted+= "was muted for" 
+				umention = user.mention
+				muted = umention
+				muted += ' was muted for '
 				muted += tint
 				muted += tdenom
+				muted += ' Reason: `'
 				muted += reason
-				mutedchannel = bot.get_channel(report)
-				await ctx.send(mutedchannel, muted)
-				tr = mute.mute(tint, tdenom)
-				if tr is 0:
-					await user.remove_roles(role)
+				muted += '`'
+				mchannel = bot.get_channel(report)
+				mlc = bot.get_channel(ml)
+				await ctx.send(muted)
+				await mchannel.send(muted)
+				await mlc.send(muted)
+				if tdenom is 's':
+					time = 1
+				elif tdenom is 'm':
+					time = 60
+				elif tdenom is 'h':
+					time = 3600
+				elif tdenom is 'd':
+					time = 86400
+				t = tint * time
+				await asyncio.sleep(t)
+				await user.remove_roles(role)
 	else:
-		bot.say('Correct usage is: >mute <@person> <time integer> <s/m/h/d> <reason(optional)>')
+		await ctx.send('Correct usage is: >mute <@person> <time integer> <s/m/h/d> <reason(optional)>')
 	
 @mute.error
 async def mute_error(ctx, error):
 	if isinstance(error, commands.errors.MissingPermissions):
 		await ctx.send(':x: You do not have permission to use this command!')
-
+	elif isinstance(error, discord.Forbidden):
+		await ctx.send(':x: Error 403: You are forbidden from using that command!')
+	else:
+		print(error)
 
 bot.run("<token>")
